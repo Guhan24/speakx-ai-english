3859746751a244123017978d74d2578c45635b2e
Guhan24 <118674327+Guhan24@users.noreply.github.com>
2026-08-18 00:41:05 +0530

    run

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Fixes white blank screen issue on GitHub Pages & subpath deployments
+  base: '/speakx-ai-english/', // Required for GitHub Pages repository deployment https://guhan24.github.io/speakx-ai-english/
   plugins: [react()],
   server: {
     port: 3000,
